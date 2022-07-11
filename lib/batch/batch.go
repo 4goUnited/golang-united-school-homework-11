76f8c2afe7f0673b84c7e25cf1a5729e6ed269b9
4goUnited <4goUnited@gmail.com>
2022-07-12 00:15:51 +0500

    Second sollution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -14,23 +14,44 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+//func getBatch(n int64, pool int64) (res []user) {
+//	var id int64 = -1
+//	var mx sync.Mutex
+//	var wg sync.WaitGroup
+//	for int64(len(res)) < n {
+//		for i := 0; int64(i) < pool; i++ {
+//			id += 1
+//			wg.Add(1)
+//			go func(wg *sync.WaitGroup, j int64) {
+//	                        defer wg.Done()
+//				u :=  getOne(j)
+//				mx.Lock()
+//				res = append(res, u)
+//				mx.Unlock()
+//			}(&wg, id)
+//		}
+//		wg.Wait()
+//	}
+//	return
+//}
+
 func getBatch(n int64, pool int64) (res []user) {
-	var id int64 = -1
+	var i int64
 	var mx sync.Mutex
 	var wg sync.WaitGroup
-	for int64(len(res)) < n {
-		for i := 0; int64(i) < pool; i++ {
-			id += 1
-			wg.Add(1)
-			go func(wg *sync.WaitGroup, j int64) {
-	                        defer wg.Done()
-				u :=  getOne(j)
-				mx.Lock()
-				res = append(res, u)
-				mx.Unlock()
-			}(&wg, id)
-		}
-		wg.Wait()
+	ch := make(chan struct{}, pool)
+	for ; i < n; i++ {
+		wg.Add(1)
+		ch <- struct{}{}
+		go func(i int64) {
+			u := getOne(i)
+			<-ch
+			mx.Lock()
+			res = append(res, u)
+			mx.Unlock()
+			wg.Done()
+		}(i)
 	}
-	return res
+	wg.Wait()
+	return
 }
